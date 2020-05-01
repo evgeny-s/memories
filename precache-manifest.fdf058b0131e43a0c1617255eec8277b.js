f7217001aed40200a13d200d0320034213c121b0
Evgeny Svirsky <evgeny@cere.io>
2020-05-01 14:55:55 +0300

    Updates

---
+++ precache-manifest.fdf058b0131e43a0c1617255eec8277b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce490938326e7fd9290f2ab7c584518b",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "9186b4a79e4e849d7291",
+    "url": "/memory/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "0cd703e81d75e80d55c8",
+    "url": "/memory/static/css/main.ae2ec01f.chunk.css"
+  },
+  {
+    "revision": "9186b4a79e4e849d7291",
+    "url": "/memory/static/js/2.b0150e1f.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/memory/static/js/2.b0150e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd703e81d75e80d55c8",
+    "url": "/memory/static/js/main.6a607ba8.chunk.js"
+  },
+  {
+    "revision": "70f7300e0526a5bfd17e",
+    "url": "/memory/static/js/runtime-main.4d1b8c16.js"
+  }
+]);
